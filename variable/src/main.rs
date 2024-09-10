1f7d8fb5c2a652c0777ce6523af17ee0023ed372
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2024-09-11 00:06:30 +0530

    add: variable/main.rs

---
+++ variable/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let age: i32 = 24;
+    age = 25;
+
+    println!("My age is" {age});
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    // Data Types
+    // Rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters.
+}
