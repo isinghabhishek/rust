a44ae84c843458d438b6c5fb965698451e464ed6
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2024-10-23 00:31:36 +0530

    add: updating guessing_game/main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -33,29 +33,3 @@ fn main() {
         }
     }
 }
-
-// fn main() {
-//     println!("Guess the number!");
-
-//     // let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
-//     // println!("Generated secret Num {secret_number}");
-
-//     let mut guess: String = String::new();
-//     // io::stdin().read_line(&mut guess).expect("Failed to read user input");
-
-// loop {
-//     println!("Please input your guess:");
-//     io::stdin()
-//         .read_line(&mut guess)
-//         .expect("Failed to read user input");
-
-//     let guess: u32 = guess.trim().parse().expect("Failed to read user input");
-
-//     match guess.cmp(&secret_number) {
-//         Ordering::Equal => println!("You won!"),
-//         Ordering::Greater => println!("Too Big"),
-//         Ordering::Less => println!("Too Small"),
-//     }
-// }
-
-// }
